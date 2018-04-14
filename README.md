24e77510237bc5285329be44dba14625c7e2dac1
Doug Anson <doug.anson@arm.com>
2018-04-14 16:49:54 -0500

    updated for R1.3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 mbedConnectorInteface endpoint example for mbed Cloud 
+
+NOTE: As of R1.3, the developer MUST link in the bootloader to use this sample
--- source/platform/mbed-os/pal_plat_rot_insecure.cpp
+++ source/platform/mbed-os/pal_plat_rot_insecure.cpp
@@ -1,28 +1,28 @@
-// ----------------------------------------------------------------------------
-// Copyright 2016-2017 ARM Ltd.
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ----------------------------------------------------------------------------
+/*
+ * Copyright (c) 2016-2017 ARM Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-
-#include "pal_plat_rtos.h"
+#include "pal.h"
 
 #define PAL_DEVICE_KEY_SIZE_IN_BYTES 16
 
 //THIS CODE IS FOR TESTING PURPOSES ONLY. DO NOT USE IN PRODUCTION ENVIRONMENTS. REPLACE WITH A PROPER IMPLEMENTATION BEFORE USE
-palStatus_t  __attribute__((weak)) pal_plat_osGetRoT128Bit(uint8_t *keyBuf, size_t keyLenBytes)
+#ifdef __cplusplus
+extern "C" {
+#endif
+palStatus_t  pal_plat_osGetRoTFromHW(uint8_t *keyBuf, size_t keyLenBytes)
 {
 #if defined (__CC_ARM)          /* ARM compiler. */
     #warning("PAL_INSECURE- You are using insecure Root Of Trust implementation, DO NOT USE IN PRODUCTION ENVIRONMENTS. REPLACE WITH A PROPER IMPLEMENTATION BEFORE USE")
@@ -50,4 +50,6 @@ palStatus_t  __attribute__((weak)) pal_plat_osGetRoT128Bit(uint8_t *keyBuf, size
 
     return PAL_SUCCESS;
 }
-
+#ifdef __cplusplus
+}
+#endif
