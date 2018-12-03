e94eb78ec2c645eb7dfd8aff38725cf8d2c9e8b5
Doug Anson <doug.anson@arm.com>
2018-12-03 13:20:42 -0600

    updated for certificate renewal annunication

--- certificate_enrollment_user_cb.h
+++ certificate_enrollment_user_cb.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 ARM Limited. All rights reserved.
+ * Copyright (c) 2015-2018 ARM Limited. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the License); you may
  * not use this file except in compliance with the License.
@@ -14,14 +14,28 @@
  * limitations under the License.
  */
 
-#ifndef __SECURITY_H__
-#define __SECURITY_H__
+///////////
+// INCLUDES
+///////////
 
-// mbed Cloud includes
-#include "mbed-cloud-client/MbedCloudClient.h"
-#include "mbed_cloud_client_user_config.h"
+#ifndef CERTIFICATE_ENROLLMENT_USER_CB_H
+#define CERTIFICATE_ENROLLMENT_USER_CB_H
 
-// Enable/Disable certificate renew callbacks
-#define ADD_CERTIFICATE_RENEW_CB  true
+#ifdef ADD_CERTIFICATE_RENEW_CB
 
-#endif // __SECURITY_H__
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void certificate_renewal_cb(const char *cert_name, ce_status_e status, ce_initiator_e initiator);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ADD_CERTIFICATE_RENEW_CB
+
+#endif // CERTIFICATE_ENROLLMENT_USER_CB_H
+
